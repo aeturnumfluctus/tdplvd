95b26a69e11c5d22cb9a1ab13a8784df9618579a
Josh Adams <joshuascottadams@gmail.com>
2021-05-25 20:20:57 -0400

    Add incrementer button

--- test/tdplvd_web/live/page_live_test.exs
+++ test/tdplvd_web/live/page_live_test.exs
@@ -8,4 +8,16 @@ defmodule TdplvdWeb.PageLiveTest do
     assert disconnected_html =~ "Welcome to Tdplvd"
     assert render(page_live) =~ "Welcome to Tdplvd"
   end
+
+  test "user can click \"Increment\" button to increment the value", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/")
+
+    assert has_element?(view, "#the-value", "0") 
+    
+    view 
+    |> element("button#value-incrementer", "Increment") 
+    |> render_click()
+
+    assert has_element?(view, "#the-value", "1") 
+  end
 end
